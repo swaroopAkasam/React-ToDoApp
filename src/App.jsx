6102641e3d26d07ff7c7bc0d5b64588894f6298b
dexur-swaroop <jyothiswaroop@dexur.com>
2024-11-20 01:25:15 +0530

    implemented delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,11 @@ export default function App() {
       })
     })    
   }
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id != id)
+    })
+  }
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -41,7 +46,7 @@ export default function App() {
               <input type="checkbox" checked={todo.completed} onChange={e => toggleTodo(todo.id, e.target.checked)} />
               {todo.title}
             </label>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
           </li>
         })}
       </ul>
